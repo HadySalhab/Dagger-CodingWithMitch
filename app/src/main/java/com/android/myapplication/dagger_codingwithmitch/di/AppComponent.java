db15fc3f6121a485c88de917a467eaf6121af4a4
Hady Salhab <hadysalhab1@gmail.com>
2020-02-14 12:55:11 +1100

    Singleton scope

--- app/src/main/java/com/android/myapplication/dagger_codingwithmitch/di/AppComponent.java
+++ app/src/main/java/com/android/myapplication/dagger_codingwithmitch/di/AppComponent.java
@@ -4,27 +4,31 @@ import android.app.Application;
 
 import com.android.myapplication.dagger_codingwithmitch.BaseApplication;
 
+import javax.inject.Singleton;
+
 import dagger.BindsInstance;
 import dagger.Component;
 import dagger.android.AndroidInjector;
 import dagger.android.support.AndroidSupportInjectionModule;
 
 //we should annotate all dagger components with @Component
-@Component (modules = {AndroidSupportInjectionModule.class,
+
+@Component(modules = {AndroidSupportInjectionModule.class,
         ActivityBuildersModule.class,
         AppModule.class
 }
 )
 //specified that baseApplication is a client
+@Singleton
 public interface AppComponent extends AndroidInjector<BaseApplication> {
     @Component.Builder
-    interface  Builder{
+    interface Builder {
 
         //this will be attached to the chain of the builder pattern
         //when we want to create an instance of the Appcomponent
         //the name can be anything
         @BindsInstance
-        Builder application (Application application);
+        Builder application(Application application);
 
         AppComponent build(); //this is mandatory
     }
--- app/src/main/java/com/android/myapplication/dagger_codingwithmitch/di/AppModule.java
+++ app/src/main/java/com/android/myapplication/dagger_codingwithmitch/di/AppModule.java
@@ -10,30 +10,37 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestManager;
 import com.bumptech.glide.request.RequestOptions;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 
 @Module
 public class AppModule {
 
+    @Singleton
     @Provides
     //gives us a request options
-    static RequestOptions provideRequestOptions(){
+    static RequestOptions provideRequestOptions() {
         return RequestOptions
                 .placeholderOf(R.drawable.white_background)
                 .error(R.drawable.white_background);
     }
+
+    @Singleton
     @Provides
     //Application is accessible from the AppComponent (@BindsInstance)
     //RequestOptions is available, because we declared its Provision
     //gives us glide instance
-    static RequestManager provideGlideInstance (Application application, RequestOptions requestOptions){
+    static RequestManager provideGlideInstance(Application application, RequestOptions requestOptions) {
         return Glide.with(application)
                 .setDefaultRequestOptions(requestOptions);
     }
+
+    @Singleton
     @Provides
-    static Drawable provideAppDrawable (Application application){
-        return ContextCompat.getDrawable(application,R.drawable.logo);
+    static Drawable provideAppDrawable(Application application) {
+        return ContextCompat.getDrawable(application, R.drawable.logo);
     }
 }
 
